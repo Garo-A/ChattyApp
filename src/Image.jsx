017a04a93b20415cc0926fb2a7e7c5a2c1e802a4
Garo-A <garo.assilian@gmail.com>
2017-07-27 18:06:37 +0000

    Refactored Image display into a diferent JSX file

---
+++ src/Image.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Image extends Component {
+  render() {
+    return (
+      <div className="message">
+        <span className="message-username" style={{color: this.props.color}}>{this.props.user}</span>
+        <img className= "image" src={this.props.content}/>
+      </div>
+      )
+  }
+}
+
+export default Image;
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Message from "./Message.jsx";
+import Image from "./Image.jsx";
 
 
 class MessageList extends Component {
@@ -10,13 +11,10 @@ class MessageList extends Component {
         { if (message.type === "IncomingMessage") {
           //CHECK IF CONTENT HAS PNG, JPG, OR GIF WITHIN URL. IF YES THEN RENDER IMG. IF NOT THEN RENDER NORMAL
           if ((message.content.includes(".jpg")) || (message.content.includes(".gif")) || (message.content.includes(".png"))) {
-            return (
-            <div className="message">
-              <span className="message-username" style={{color: message.color}}>{message.username}</span>
-              <img className= "image" src={message.content}/>
-            </div>)
+            return (<Image key={message.id} user={message.username} content={message.content} color={message.color}/>)
           } else {
             return <Message key={message.id} user={message.username} content={message.content} color={message.color}/> }
+
         } else if (message.type === "IncomingNotification"){
             return <div key={message.id} className="message system">{message.content}</div>
          }
